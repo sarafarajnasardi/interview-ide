d43bfdafcf6c81d9c21429369d06d3c3a4b6391f
Sarafaraj Nasardi <sarafarajnasardi786@gmail.com>
2026-08-17 11:07:17 +0530

    Fix ASGI settings initialization

--- interview-backend/config/asgi.py
+++ interview-backend/config/asgi.py
@@ -4,11 +4,11 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
-import submissions.routing
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 django_asgi_app = get_asgi_application()
+
+import submissions.routing
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
